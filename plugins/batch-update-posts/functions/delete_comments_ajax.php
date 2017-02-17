5045110ca2711525152f7faf99110827408b26f7
polive31 <psc.olv@gmail.com>
2017-02-17 08:32:41 +0100

    Deleted comments functional !

--- plugins/batch-update-posts/functions/delete_comments_ajax.php
+++ plugins/batch-update-posts/functions/delete_comments_ajax.php
@@ -16,46 +16,55 @@ if ( !defined('ABSPATH') )
 /* =================================================================*/
 function ajax_batch_delete_comments() {
 	
-	echo 'In Batch Delete Comments function...';
+	echo '<p>In Batch Delete Comments function...</p>';
 		
 	if ( isset($_POST['args']['post-type']) ) {
 		$post_type = $_POST['args']['post-type'];
 		echo sprintf("<b>Post type</b> = %s",$post_type);
-		//echo "<br>";
+		echo "<br>";
 	}
 		
 	if ( isset($_POST['args']['include']) ) {
 		$include = $_POST['args']['include'];
 		echo sprintf("<b>Limit to Posts</b> = %s",$include);
-		//echo "<br>";
+		echo "<br>";
 	}
 	
 
-	//echo sprintf("Limit to posts = %s", $include);
+//	$response = array('msg'=>'Dans Batch Delete Comments script',
+//										'post-type'=>$post_type,
+//										'include'=>$include,
+//										);
+//	echo json_encode( $response );
 
-	//$response = array('msg'=>'Dans Batch Delete Comments script',
-	//									'post-type'=>$post_type,
-	//									'include'=>$include,
-	//									);
-	//echo json_encode( $response );
+	$deleted_count='0';
 
-	//$post_type_object = get_post_type_object($post_type);
-	//$label = $post_type_object->label;
+	if ( ! empty($include) ) {
+		$include = ($include=='all')?'':$include;
+	 	$posts = get_posts(array('include'=>$include, 'post_type'=> $post_type, 'post_status'=> 'publish', 'suppress_filters' => false, 'posts_per_page'=>-1));
+	  foreach ($posts as $post) {
+			$comments = get_comments( array('post_id'=>$post->ID ) );
+			echo sprintf(__('Post %s contains %d comments'), $post->post_title, count($comments) );
+			echo "<br>";
+			if ( ! empty( $comments ) ) {
+				foreach ($comments as $comment) {
+					$deleted = wp_delete_comment( $comment->comment_ID );
+					if ( $deleted ) {
+						++$deleted_count;
+					}
+				}
+			}
 
-	//if ( ! empty($include) || ( $include == 'all') ) {
-	// 	$posts = get_posts(array('include'=>$include, 'post_type'=> $post_type, 'post_status'=> 'publish', 'suppress_filters' => false, 'posts_per_page'=>-1));
-	//  foreach ($posts as $post) {
-	//		$comments = get_comments( array('post_id'=>$post->ID ) );
-	//		echo sprintf(__('Post %s contains %d comments'), $post->post_title, count($comments) );
-	//		echo "<br>";
-	//		
-	//	}
-	//	
-	//}
-	//
-	//else {
-	//	echo "Please provide post IDs or 'all' for deletion to take place";
-	//} 
+		}
+		
+	}
+	
+	else {
+		echo "Please provide post IDs or 'all' for deletion to take place";
+	} 
+	
+	echo sprintf('Delete comments operation completed, %s comments deleted',$deleted_count);
+	echo "<br>";
 
 }
 
--- plugins/batch-update-posts/js/ajax_call_on_button_press.js
+++ plugins/batch-update-posts/js/ajax_call_on_button_press.js
@@ -6,15 +6,17 @@ jQuery(document).ready(function(){
   	var ScriptName = jQuery(this).data('name');
   	var ScriptInst = jQuery(this).data('instance');
   	
-		console.log('Lancement du script = '  + 'script' + ScriptName + ScriptInst);
+		//console.log('Lancement du script = '  + 'script' + ScriptName + ScriptInst);
 		
   	// Récupération des données de WPLocalize propres au shortcode sélectionné
 		var WPLocalizeVar = window['script' + ScriptName + ScriptInst];
 		var ShortcodeArgs = jQuery.parseJSON( WPLocalizeVar.data );
 		var AjaxURL = WPLocalizeVar.url;
 		
-		console.log('Arguments du shortcode = %0',ShortcodeArgs);
-		console.log('URL de la page = '  + AjaxURL);
+		//console.log('Arguments du shortcode = %0',ShortcodeArgs);
+		//console.log('URL de la page = '  + AjaxURL);
+		
+		jQuery('div#resp' + ScriptName + ScriptInst).html( 'Ajax call launched...' );
 
 		jQuery.post(
 		 AjaxURL,
@@ -24,10 +26,10 @@ jQuery(document).ready(function(){
 			dataType: "text",
 		 },
 		function( response ) {
-		 		console.log( 'Ajax call successfull');
-		 		console.log('#resp' + ScriptName + ScriptInst);
-		 		console.log( response );
-		 		jQuery('#resp' + ScriptName + ScriptInst).html( response );
+		 		//console.log( 'Ajax call successfull');
+		 		//console.log('div#resp' + ScriptName + ScriptInst);
+		 		//console.log( response );
+		 		jQuery('div#resp' + ScriptName + ScriptInst).html( response );
 		});
 		
   });

814e331cd8808934dae5b35e9150edbc6770a7d3
polive31 <psc.olv@gmail.com>
2017-03-05 20:54:44 +0100

    updated

--- plugins/custom_shortcodes/custom-gallery-shortcode.php
+++ plugins/custom_shortcodes/custom-gallery-shortcode.php
@@ -1,8 +1,8 @@
 <?php
 /*
-Plugin Name: Misc Shortcodes
+Plugin Name: Custom Gallery Shortcode
 Plugin URI: http://goutu.org
-Description: Provides misc shortcodes  
+Description: Provides customized gallery shortcode
 Version: 1.0
 Author: Pascal Olive 
 Author URI: http://goutu.org
@@ -14,54 +14,13 @@ if ( !defined('ABSPATH') )
 	die('-1');
 	
 
-/* Language switcher
---------------------------------------------- */
-	
-//function display_language_switcher() {
-//	if( !function_exists('pll_the_languages')
-//			return 'Polylang not installed';		
-//		$lang = pll_the_languages( array( 'echo' => 0,'raw' => 1, 'hide_current' => 1 ));
-//	foreach ($lang as $thislang) {
-//		if (!$thislang->current_lang)
-//			$html .= '<a href="' . $lang->url .'"> ' . $lang->name . '</a>';
-//	}
-//	return $html;
-//}
-//
-//add_shortcode('language', 'display_language_switcher'); 
+/* Suppression de la feuille de style de la gallerie Wordpress */
+add_filter( 'use_default_gallery_style', '__return_false' );
+add_shortcode('custom-gallery', 'custom_gallery_shortcode');
+add_action( 'wp_enqueue_scripts', function() {
+			wp_enqueue_style( 'custom-gallery-stylesheet', 'custom-gallery-stylesheet.css', array(), CHILD_THEME_VERSION );		
+} );
 
-/* Add Comment Form 
------------------------------------------------*/
-function add_comment_form_shortcode() {
-//		$comments_args = array( 
-//			'title_reply' => __( '', 'genesis' ), 
-//      'comment_field'=>'<p class="comment-form-comment"></p>', 
-//		);
-		$comment_args='';
-    ob_start();
-    comment_form($comment_args);
-    $cform = ob_get_contents();
-    ob_end_clean();
-    return $cform;
- }
-add_shortcode( 'add-comment-form', 'add_comment_form_shortcode' );
-
-
-/* Share Title Output
---------------------------------------------- */
-	
-function display_share_title() {
-	if (is_singular()) {
-		if (is_singular('recipe')) 
-			$msg=__('Share this recipe','foodiepro');
-		else
-			$msg=__('Share this post','foodiepro');
-		$html = '<h3 class="share-title">' . $msg . '</h3>';
-	}
-	return $html;
-}
-
-add_shortcode('share-title', 'display_share_title'); 
 
 /**
  * Builds the Gallery shortcode output.
@@ -225,8 +184,4 @@ function custom_gallery_shortcode( $attr ) {
 	return $output;
 }
 
-add_shortcode('custom-gallery', 'custom_gallery_shortcode');
 
-
-
-?>

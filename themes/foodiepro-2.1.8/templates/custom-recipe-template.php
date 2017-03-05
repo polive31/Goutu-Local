a38836e05485a63c2785e06604d404ff22b898ff
polive31 <psc.olv@gmail.com>
2017-03-05 14:31:46 +0100

    Added JSON+LD metadata to recipe + ratings

--- themes/foodiepro-2.1.8/templates/custom-recipe-template.php
+++ themes/foodiepro-2.1.8/templates/custom-recipe-template.php
@@ -6,17 +6,21 @@ function wpurp_custom_recipe_template( $content, $recipe ) {
 	
 	$post_ID = get_the_ID();
 
+	// Output JSON+LD metadata & rich snippets
+		echo json_ld_meta_output($recipe,'');
 	?>
 
 	<div class="recipe wpurp-container">
 	<!-- Class .wpurp-container important for adjustable servings javascript -->	
 
+		<!-- Recipe description -->
 		<div class="recipe-container">
 			<?php
 			echo $recipe->description();
 			?>	
 		</div>
 			
+		<!-- Function buttons  -->
 		<div class="recipe-top">
 				<div class="recipe-buttons">
 				
@@ -63,6 +67,7 @@ function wpurp_custom_recipe_template( $content, $recipe ) {
 			
 		</div>
 		
+		<!-- Image + recipe info -->
 		<div class="recipe-container">
 			
 			<div class="image-container">
@@ -130,6 +135,7 @@ function wpurp_custom_recipe_template( $content, $recipe ) {
 			
 		</div>
 		
+		<!-- Ingredients + Instructions -->
 		<div class="recipe-container">
 			
 			<div class="ingredients-container"> 
@@ -167,6 +173,26 @@ function wpurp_custom_recipe_template( $content, $recipe ) {
 	<?php
     $output = ob_get_contents();
     ob_end_clean();
+
+	return $output;
+}
+
+
+function json_ld_meta_output( $recipe, $args ) {
+	
+	$WPURP_Custom_Metadata = new WPURP_Custom_Metadata;
+	$metadata = in_array( WPUltimateRecipe::option( 'recipe_metadata_type', 'json-inline' ), array( 'json', 'json-inline' ) ) ? $WPURP_Custom_Metadata->get_metadata( $recipe ) : '';
+	$meta = WPUltimateRecipe::option( 'recipe_metadata_type', 'json-inline' ) != 'json' ? ' itemscope itemtype="http://schema.org/Recipe"' : '';
+
+	ob_start();?>
+
+	<div<?php echo $meta; ?> id="wpurp-container-recipe-<?php echo $recipe->ID(); ?>" data-id="<?php echo $recipe->ID(); ?>" data-permalink="<?php echo $recipe->link(); ?>" data-image="<?php echo $image_url; ?>" data-servings-original="<?php echo $recipe->servings_normalized(); ?>">
+
+	<?php
+	echo $metadata;
+
+	$output = ob_get_contents();
+  ob_end_clean();
 
 	return $output;
 }
@@ -453,6 +479,171 @@ class WPURP_Custom_Recipe_Favorite extends WPURP_Template_Block {
 
         return $this->after_output( $output, $recipe, $args );
     }
+}
+
+class WPURP_Custom_Metadata {
+
+    public function get_metadata( $recipe )
+    {
+        if( is_feed() ) {
+            return '';
+        }
+        
+        $metadata = $this->get_metadata_array( $recipe );
+        $metadata = $this->sanitize_metadata( $metadata );
+        return '<script type="application/ld+json">' . json_encode( $metadata ) . '</script>';
+    }
+
+    private function sanitize_metadata( $metadata ) {
+		$sanitized = array();
+		if ( is_array( $metadata ) ) {
+			foreach ( $metadata as $key => $value ) {
+				$sanitized[ $key ] = $this->sanitize_metadata( $value );
+			}
+		} else {
+			$sanitized = strip_shortcodes( wp_strip_all_tags( $metadata ) );
+		}
+		return $sanitized;
+	}
+
+    private function get_metadata_array( $recipe )
+    {
+        $recipe = is_null( $recipe ) ? new WPURP_Recipe(0) : $recipe;
+        $post_id = get_the_id();
+
+        // Essentials
+        $metadata = array(
+            '@context' => 'http://schema.org/',
+            '@type' => 'Recipe',
+            'name' => $recipe->title(),
+            'author' => array(
+                '@type' => 'Person',
+                'name' => $recipe->author(),
+            ),
+            'datePublished' => $recipe->date(),
+            'image' => $recipe->image_url( 'full' ),
+            'description' => $recipe->description(),
+        );
+
+
+        // Yield
+        if( $recipe->servings() ) $metadata['recipeYield'] = $recipe->servings() . ' ' . $recipe->servings_type();
+
+
+        // Rating
+				//$metadata['aggregateRating'] = do_shortcode('[json-ld-rating]');
+				$rating_stats = explode(' ', do_shortcode('[json-ld-rating]') );
+        $metadata['aggregateRating'] = array(
+            '@type' => 'AggregateRating',
+            'ratingValue' => $rating_stats[0],
+            'ratingCount' => $rating_stats[1],
+        );
+
+        // Times
+        if( $recipe->prep_time_meta() && $recipe->cook_time_meta() ) {
+            // Only use separate ones when we have both
+            $metadata['prepTime'] = $recipe->prep_time_meta();
+            $metadata['cookTime'] = $recipe->cook_time_meta();
+        } else {
+            // Otherwise use total time
+            if( $recipe->prep_time_meta() ) $metadata['totalTime'] = $recipe->prep_time_meta();
+            if( $recipe->cook_time_meta() ) $metadata['totalTime'] = $recipe->cook_time_meta();
+        }
+
+
+        // Nutrition
+        if( WPUltimateRecipe::is_addon_active( 'nutritional-information' ) ) {
+            $nutritional = $recipe->nutritional();
+            $nutritional_units = WPUltimateRecipe::addon( 'nutritional-information' )->fields;
+            $nutritional_units['unsaturated_fat'] = 'g';
+
+            $mapping = array(
+                'calories' => 'calories',
+                'fat' => 'fatContent',
+                'saturated_fat' => 'saturatedFatContent',
+                'unsaturated_fat' => 'unsaturatedFatContent',
+                'trans_fat' => 'transFatContent',
+                'carbohydrate' => 'carbohydrateContent',
+                'sugar' => 'sugarContent',
+                'fiber' => 'fiberContent',
+                'protein' => 'proteinContent',
+                'cholesterol' => 'cholesterolContent',
+                'sodium' => 'sodiumContent',
+            );
+
+            // Unsaturated Fat = mono + poly
+            if( isset( $nutritional['monounsaturated_fat'] ) && $nutritional['monounsaturated_fat'] !== '' ) {
+                $nutritional['unsaturated_fat'] = floatval( $nutritional['monounsaturated_fat'] );
+            }
+
+            if( isset( $nutritional['polyunsaturated_fat'] ) && $nutritional['polyunsaturated_fat'] !== '' ) {
+                $mono = isset( $nutritional['unsaturated_fat'] ) ? $nutritional['unsaturated_fat'] : 0;
+                $nutritional['unsaturated_fat'] = $mono + floatval( $nutritional['polyunsaturated_fat'] );
+            }
+
+            // Get metadata
+            $metadata_nutrition = array(
+                '@type' => 'NutritionInformation',
+                'servingSize' => '1 serving',
+            );
+
+            foreach( $mapping as $field => $meta_field ) {
+                if( isset( $nutritional[$field] ) && $nutritional[$field] !== '' ) {
+                    $metadata_nutrition[$meta_field] = floatval( $nutritional[$field] ) . ' ' . $nutritional_units[$field];
+                }
+            }
+
+            if( count( $metadata_nutrition ) > 2 ) {
+                $metadata['nutrition'] = $metadata_nutrition;
+            }
+        }
+
+
+        // Ingredients
+        if( $recipe->has_ingredients() ) {
+            $metadata_ingredients = array();
+
+            foreach( $recipe->ingredients() as $ingredient ) {
+                $metadata_ingredient = $ingredient['amount'] . ' ' . $ingredient['unit'] . ' ' . $ingredient['ingredient'];
+                if( trim( $ingredient['notes'] ) !== '' ) {
+                    $metadata_ingredient .= ' (' . $ingredient['notes'] . ')';
+                }
+
+                $metadata_ingredients[] = $metadata_ingredient;
+            }
+
+            $metadata['recipeIngredient'] = $metadata_ingredients;
+        }
+
+
+        // Instructions
+        if( $recipe->has_instructions() ) {
+            $metadata_instructions = array();
+
+            foreach( $recipe->instructions() as $instruction ) {
+                $metadata_instructions[] = $instruction['description'];
+            }
+
+            $metadata['recipeInstructions'] = $metadata_instructions;
+        }
+
+
+        // Category & Cuisine
+        $courses = wp_get_post_terms( $recipe->ID(), 'course', array( 'fields' => 'names' ) );
+        if( !is_wp_error( $courses ) && isset( $courses[0] ) ) {
+            $metadata['recipeCategory'] = $courses[0];
+        }
+
+        $cuisines = wp_get_post_terms( $recipe->ID(), 'cuisine', array( 'fields' => 'names' ) );
+        if( !is_wp_error( $cuisines ) && isset( $cuisines[0] ) ) {
+            $metadata['recipeCuisine'] = $cuisines[0];
+        }
+
+
+        // Allow external filtering of metadata
+        return apply_filters( 'wpurp_custom_recipe_metadata', $metadata, $recipe );
+    } 
+    
 }
     
 ?>

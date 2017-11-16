f7f4c4ff33c87142e1b0da0f100a11679ebc3917
polive31 <psc.olv@gmail.com>
2017-11-16 01:13:28 +0100

    Improved overlay

--- plugins/custom-navigation-helpers/includes/CustomArchiveEntryTitle.php
+++ plugins/custom-navigation-helpers/includes/CustomArchiveEntryTitle.php
@@ -34,37 +34,37 @@ class CustomArchiveEntryTitle extends CustomArchive {
 			$post_id = get_the_ID();
 			$origin = wp_get_post_terms( $post_id, 'cuisine', array("fields" => "names"));
 			$diet = wp_get_post_terms( $post_id, 'diet');
-			//$season = wp_get_post_terms( $post_id, 'season');
+			$season = wp_get_post_terms( $post_id, 'season');
 
-			$title = $this->output_tags($origin[0], $diet[0]->slug, '', ''/*$season[0]->slug*/) . $title;
+			$title = $this->output_tags($origin[0], $diet[0]->slug, '', $season[0]->slug) . $title;
 		};
 		
 		if ( is_tax('course') ) {
 			$post_id = get_the_ID();
 			
-			$occasion = wp_get_post_terms( $post_id, 'occasion', array("fields" => "names"));
+			$occasion = wp_get_post_terms( $post_id, 'occasion');
 			$season = wp_get_post_terms( $post_id, 'season');
 			$diet = wp_get_post_terms( $post_id, 'diet');
 			
-			$title = $this->output_tags('', $diet[0]->slug, $occasion[0], $season[0]->slug) . $title;
+			$title = $this->output_tags('', $diet[0]->slug, $occasion, $season[0]->slug) . $title;
 		};	
 		
 		if ( is_tax('season') ) {
 			$post_id = get_the_ID();
 			
 			$diet = wp_get_post_terms( $post_id, 'diet');
-			$occasion = wp_get_post_terms( $post_id, 'occasion', array("fields" => "names"));
+			$occasion = wp_get_post_terms( $post_id, 'occasion');
 			
-			$title = $this->output_tags('', $diet[0]->slug, $occasion[0], '') . $title;
+			$title = $this->output_tags('', $diet[0]->slug, $occasion, '') . $title;
 		};
 		
 		if ( is_tax('diet') ) {
 			$post_id = get_the_ID();
 			
 			$season = wp_get_post_terms( $post_id, 'season');
-			$occasion = wp_get_post_terms( $post_id, 'occasion', array("fields" => "names"));
+			$occasion = wp_get_post_terms( $post_id, 'occasion');
 			
-			$title = $this->output_tags('', '', $occasion[0], $season[0]->slug) . $title;
+			$title = $this->output_tags('', '', $occasion, $season[0]->slug) . $title;
 		};						
 
 		if ( is_tax('occasion') ) {
@@ -72,8 +72,19 @@ class CustomArchiveEntryTitle extends CustomArchive {
 			$diet = wp_get_post_terms( $post_id, 'diet');
 			$season = wp_get_post_terms( $post_id, 'season');
 			
-			$title = $this->output_tags( $diet[0]->slug, '', $season[0]->slug) . $title;
-		};			
+			$title = $this->output_tags( '', $diet[0]->slug, '', $season[0]->slug) . $title;
+		};	
+
+		if ( is_tax('ingredient') ) {
+			
+			$post_id = get_the_ID();
+			$origin = wp_get_post_terms( $post_id, 'cuisine', array("fields" => "names"));
+			$diet = wp_get_post_terms( $post_id, 'diet');
+			$season = wp_get_post_terms( $post_id, 'season');
+			$occasion = wp_get_post_terms( $post_id, 'occasion');
+			
+			$title = $this->output_tags( $origin[0], $diet[0]->slug, $occasion, $season[0]->slug) . $title;
+		};					
 
 		return $title;
 	}
@@ -81,7 +92,8 @@ class CustomArchiveEntryTitle extends CustomArchive {
 
 	public function output_tags($origin, $diet, $occasion, $season) {
 		
-		$season_msg = __('Seasonal !', 'foodiepro');
+		$season_msg = __('Seasonal', 'foodiepro');
+		$fest_msg = __('Festive', 'foodiepro');
 		//$veg_msg = __('Veggie', 'foodiepro');
 		$veg_msg = '<i class="fa fa-leaf" aria-hidden="true"></i>';
 		$tags = '';
@@ -89,24 +101,26 @@ class CustomArchiveEntryTitle extends CustomArchive {
 		$left_id=0;
 		$right_id=0;
 		
-		if ( $origin!='' ) {
-			$tags .= '<div class="overlay" id="left' . $left_id . '">' . $origin . '</div>';
-			$left_id++;
-		}			
 
 		if ( $this->is_veg($diet) ) {
 			//$tags .= '<div class="overlay" id="veg">' . $veg_msg . '</div>';
 			$tags .= '<div class="overlay" id="veg">' . $veg_msg . '</div>';
 			$right_id++;
 		}
-				
-		if ($this->is_season($season)) {
-			$tags .= '<div class="overlay" id="right' . $right_id . '">' . $season_msg . '</div>';
-			$right_id++;
+		
+		if ( $origin!='' ) {
+			$tags .= '<div class="overlay" id="left' . $left_id . '">' . $origin . '</div>';
+			$left_id++;		
 		}
 		
-		if ( $occasion != '' ) {
-			$tags .= '<div class="overlay" id="left' . $left_id . '">' . $occasion . '</div>';
+		if ($this->is_season($season)) {
+			$tags .= '<div class="overlay col-' . $season[0] . '" id="left' . $left_id . '">' . $season_msg . '</div>';
+			$left_id++;
+		}
+		
+		//print_r($occasion);
+		if ($this->is_fest($occasion)) {
+			$tags .= '<div class="overlay" id="left' . $left_id . '">' . $fest_msg . '</div>';
 			$left_id++;
 		}	
 		

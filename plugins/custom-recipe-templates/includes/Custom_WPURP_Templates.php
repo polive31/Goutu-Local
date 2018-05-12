9c4f1b92be6a7a4a38a44b0de01fe76b93cfa94b
Pascal <psc.olv@gmail.com>
2018-05-12 17:52:57 +0200

    Improved custom recipe scripts & styles loading (version control)

--- plugins/custom-recipe-templates/includes/Custom_WPURP_Templates.php
+++ plugins/custom-recipe-templates/includes/Custom_WPURP_Templates.php
@@ -10,10 +10,12 @@ class Custom_WPURP_Templates {
 	const RECIPES_LIST_SLUG = 'publier-recettes';
 	const RECIPE_NEW_SLUG = 'nouvelle-recette';
 	const RECIPE_EDIT_SLUG = 'modifier-recette';
+	const CUSTOM_WPURP_TEMPLATES_VERSION = '0.0.1';
 	protected static $_PluginPath;	
 	protected static $_PluginUri;	
 	protected $logged_in;
 	protected $custom_enqueued_scripts = array();
+	protected $custom_enqueued_styles = array();
 	
 	public function __construct() {
 	
@@ -22,13 +24,13 @@ class Custom_WPURP_Templates {
 		
 		add_action( 'init', array($this, 'hydrate'));
 		
-		/* Load javascript styles */
+		/* WPURP enqueue list */
 		add_filter ( 'wpurp_assets_js', array($this,'enqueue_wpurp_js'), 15, 1 );
-		add_action( 'wp_enqueue_scripts', array($this, 'custom_wpurp_scripts_enqueue') );
-		
-		/* Load stylesheets */
 		add_filter ( 'wpurp_assets_css', array($this,'enqueue_wpurp_css'), 15, 1 );
-
+		
+		/* Custom enqueue list */
+		add_action( 'wp_enqueue_scripts', array($this, 'custom_wpurp_scripts_styles_enqueue') );
+		
 
 		/* Customize User Submission shortcode */
 		// add_filter ( 'wpurp_user_submissions_current_user_edit_item', array($this, 'remove_recipe_list_on_edit_recipe'), 15, 2 );
@@ -61,35 +63,34 @@ class Custom_WPURP_Templates {
 		if ( is_admin() ) return $css_enqueue;
 			
 		if ( is_singular('recipe') ) {
-		  	$css_enqueue=array(
-					array(
-						'url' => self::$_PluginUri . 'assets/css/custom-recipe.css',
-						'public' => true,
-					),
-					// array(
-		   //            'url' => self::$_PluginPath . 'assets/css/tooltips.css',
-		   //            'public' => true,
-		   //        	),
-					array(
-		              'url' => '//fonts.googleapis.com/css?family=Cabin',
-		              'public' => true,
-		          	),				                             
+		  	$css_enqueue=array();
+		  	$this->custom_enqueued_styles=array(
+				array(
+					'url' => self::$_PluginUri . 'assets/css/custom-recipe.css',
+					'path' => self::$_PluginPath . 'assets/css/custom-recipe.css',
+					'public' => true,
+				),
+				array(
+					'url' => '//fonts.googleapis.com/css?family=Cabin',
+					'public' => true,
+	          	),				                             
 			);
 		}
 		elseif ( is_page( 'menus' ) ) { // Menu page
-		  $css_enqueue=array(
-							array(
-						'name' => 'custom-menu-styles',		
-		              'url' => self::$_PluginUri . 'assets/css/custom-menu.css',
-		              'public' => true,
-		          ),
+		  	$css_enqueue=array();
+		  	$this->custom_enqueued_styles=array(
+				array(
+					'name' => 'custom-menu-styles',		
+					'url' => self::$_PluginUri . 'assets/css/custom-menu.css',
+					'path' => self::$_PluginPath . 'assets/css/custom-menu.css',
+					'public' => true,
+		        ),
 			);		
 		}
 		
 		elseif ( is_page( [self::RECIPE_NEW_SLUG, self::RECIPE_EDIT_SLUG] ) ) {
 //		  $css_enqueue=null;
-//		  $css_enqueue[]=
-//							array(
+//		  $css_enqueue[]=array(
 //		              'name' => 'jquery-ui',
 //		              'file' => '//ajax.googleapis.com/ajax/libs/jqueryui/1.10.4/themes/smoothness/jquery-ui.css',
 //		              'direct' => true,
@@ -97,34 +98,29 @@ class Custom_WPURP_Templates {
 //		              'url' => '//ajax.googleapis.com/ajax/libs/jqueryui/1.10.4/themes/smoothness/jquery-ui.css',
 //		              'dir' => '//ajax.googleapis.com/ajax/libs/jqueryui/1.10.4/themes/smoothness/jquery-ui.css',
 //		          ); 
-//		  $css_enqueue[]=
-//							array(
+//		  $css_enqueue[]=array(
 //		              'file' =>  self::$_PluginPath . 'vendor/select2/css/select2.min.css',
 //		              'url' =>  self::$_PluginPath . 'vendor/select2/css/select2.min.css',
 //		              'dir' =>  self::$_PluginPath . 'vendor/select2/css/select2.min.css',
 //		              'public' => true,
 //		              'direct' => true,
 //		          ); 			
-		  	$css_enqueue[]=array(
-						'url' => self::$_PluginUri . 'assets/css/custom-recipe.css',
-						'public' => true,
-					);	
-			$css_enqueue[]=						
+			$this->custom_enqueued_styles=array(
 					array(
-		              'url' => '//fonts.googleapis.com/css?family=Cabin',
-		              'public' => true,
-		          	);						  	
-		  	$css_enqueue[]=array(
+						'url' => '//fonts.googleapis.com/css?family=Cabin',
+						'public' => true,
+		          	),
+		  			array(
 						'url' => self::$_PluginUri . 'assets/css/custom-recipe-submission.css',
 						'dir' => self::$_PluginPath . 'assets/css/custom-recipe-submission.css',
 						'public' => true,
 						'direct' => true,
-		          	);	          		
+		          	)
+		        );	          		
 		}
 		else 
 		  $css_enqueue=array();
 			
-		
 		//echo '<pre style="margin-top:200px">' . print_r($css_enqueue,true) . '</pre>';
 		return $css_enqueue;
 	}
@@ -137,113 +133,106 @@ class Custom_WPURP_Templates {
 			if ( is_singular('recipe') ) {
 				
 				$js_enqueue=array();		
-				$min_js=self::$_PluginPath . 'assets/js/custom-recipe-tools.min.js';
-				
-				// if ( file_exists( $min_js ) ) {
-				// 	$js_enqueue[] = array(
-			 //            'name' => 'custom-recipe-tools-minified',
-			 //            'url' => $min_js,
-			 //            'public' => true,
-			 //            'admin' => true,					
-				// 	);
-				// }
-				// else {
 					
-					$pause = '<i class="fa fa-pause" aria-hidden="true"></i>';
-					$play = '<i class="fa fa-play" aria-hidden="true"></i>';
-					$close = '<i class="fa fa-times" aria-hidden="true"></i>';
+				$pause = '<i class="fa fa-pause" aria-hidden="true"></i>';
+				$play = '<i class="fa fa-play" aria-hidden="true"></i>';
+				$close = '<i class="fa fa-times" aria-hidden="true"></i>';
 
-						
-			    	$js_enqueue=array(
-			            array(
-			                'name' => 'fraction',
-			                'url' => WPUltimateRecipe::get()->coreUrl . '/vendor/fraction-js/index.js',
-			                'public' => true,
-			                'admin' => true,
-			            ),
+					
+		    	$js_enqueue=array(
+		            array(
+		                'name' => 'fraction',
+		                'url' => WPUltimateRecipe::get()->coreUrl . '/vendor/fraction-js/index.js',
+		                'public' => true,
+		                'admin' => true,
+		            ),
+		            array(
+		                'name' => 'print_button',
+		                'url' => WPUltimateRecipe::get()->coreUrl . '/js/print_button.js',
+		                'public' => true,
+		                'deps' => array(
+		                    'jquery',
+		                ),
+		                'data' => array(
+		                    'name' => 'wpurp_print',
+		                    'ajaxurl' => WPUltimateRecipe::get()->helper('ajax')->url(),
+		                    'nonce' => wp_create_nonce( 'wpurp_print' ),
+		                    'custom_print_css_url' => get_stylesheet_directory_uri() . '/assets/css/custom-recipe-print.css',
+		                    'coreUrl' => WPUltimateRecipe::get()->coreUrl,
+		                    'premiumUrl' => WPUltimateRecipe::is_premium_active() ? WPUltimateRecipePremium::get()->premiumUrl : false,
+		                    'title' => __('Print this Recipe','foodiepro'),
+		                    'permalinks' => get_option('permalink_structure'),
+		                ),
+		            ),
+		    	    array(
+		                'name' => 'adjustable-servings',
+		                'url' => WPUltimateRecipe::get()->coreUrl . '/js/adjustable_servings.js',
+		                'public' => true,
+		                'deps' => array(
+		                    'jquery',
+		                    'fraction',
+		                		'print_button',
+		                ),
+		                'data' => array(
+		                    'name' => 'wpurp_servings',
+		                    'precision' => 1,
+		                    'decimal_character' => ',',
+		                ),
+		            ),
+		            array(
+		                'name' => 'wpurp-timer',
+		                'url' => WPUltimateRecipePremium::get()->premiumUrl . '/addons/timer/js/timer.js',
+		                'premium' => true,
+		                'public' => true,
+		                'deps' => array(
+		                    'jquery',
+		                ),
+		                'data' => array(
+		                    'name' => 'wpurp_timer',
+		                    'icons' => array(
+		                        'pause' => $pause,
+		                        'play' => $play,
+		                        'close' => $close,
+		                    ),
+		                )
+		            ),		            
+	    		);	
 
-			            array(
-			                'name' => 'print_button',
-			                'url' => WPUltimateRecipe::get()->coreUrl . '/js/print_button.js',
-			                'public' => true,
-			                'deps' => array(
-			                    'jquery',
-			                ),
-			                'data' => array(
-			                    'name' => 'wpurp_print',
-			                    'ajaxurl' => WPUltimateRecipe::get()->helper('ajax')->url(),
-			                    'nonce' => wp_create_nonce( 'wpurp_print' ),
-			                    'custom_print_css_url' => get_stylesheet_directory_uri() . '/assets/css/custom-recipe-print.css',
-			                    'coreUrl' => WPUltimateRecipe::get()->coreUrl,
-			                    'premiumUrl' => WPUltimateRecipe::is_premium_active() ? WPUltimateRecipePremium::get()->premiumUrl : false,
-			                    'title' => __('Print this Recipe','foodiepro'),
-			                    'permalinks' => get_option('permalink_structure'),
-			                ),
-			            ),
-			    	    array(
-			                'name' => 'adjustable-servings',
-			                'url' => WPUltimateRecipe::get()->coreUrl . '/js/adjustable_servings.js',
-			                'public' => true,
-			                'deps' => array(
-			                    'jquery',
-			                    'fraction',
-			                		'print_button',
-			                ),
-			                'data' => array(
-			                    'name' => 'wpurp_servings',
-			                    'precision' => 1,
-			                    'decimal_character' => ',',
-			                ),
-			            ),
-						array(
-			                'name' => 'custom-favorite-recipe',
-			                /*'url' => WPUltimateRecipePremium::get()->premiumUrl . '/addons/favorite-recipes/js/favorite-recipes.js',*/
-			                'url' => self::$_PluginUri . 'assets/js/custom_favorite_recipe.js',
-			               	'premium' => true,
-			                'public' => true,
-			                'setting' => array( 'favorite_recipes_enabled', '1' ),
-			                'deps' => array(
-			                    'jquery',
-			                ),
-			                'data' => array(
-			                    'name' => 'wpurp_favorite_recipe',
-			                    'ajaxurl' => WPUltimateRecipe::get()->helper('ajax')->url(),
-			                    'nonce' => wp_create_nonce( 'wpurp_favorite_recipe' ),
-			                )
-			            ),	  
-			            array(
-			                /*'url' => WPUltimateRecipePremium::get()->premiumUrl . '/js/add-to-shopping-list.js',*/
-			                'name' => 'custom-shopping-list',
-			                'url' => self::$_PluginUri . 'assets/js/custom_shopping_list.js',
-			                'premium' => true,
-			                'public' => true,
-			                'deps' => array(
-			                    'jquery',
-			                ),
-			                'data' => array(
-			                    'name' => 'wpurp_add_to_shopping_list',
-			                    'ajaxurl' => WPUltimateRecipe::get()->helper('ajax')->url(),
-			                    'nonce' => wp_create_nonce( 'wpurp_add_to_shopping_list' ),
-			                )
-			            ),
-			            array(
-			                'name' => 'wpurp-timer',
-			                'url' => WPUltimateRecipePremium::get()->premiumUrl . '/addons/timer/js/timer.js',
-			                'premium' => true,
-			                'public' => true,
-			                'deps' => array(
-			                    'jquery',
-			                ),
-			                'data' => array(
-			                    'name' => 'wpurp_timer',
-			                    'icons' => array(
-			                        'pause' => $pause,
-			                        'play' => $play,
-			                        'close' => $close,
-			                    ),
-			                )
-			            ),		            
-		    		);	
+				$this->custom_enqueued_scripts = array (
+					array(
+		                'name' => 'custom-favorite-recipe',
+		                /*'url' => WPUltimateRecipePremium::get()->premiumUrl . '/addons/favorite-recipes/js/favorite-recipes.js',*/
+		                'url' => self::$_PluginUri . 'assets/js/custom_favorite_recipe.js',
+		                'path' => self::$_PluginPath . 'assets/js/custom_favorite_recipe.js',
+		               	// 'premium' => true,
+		                'public' => true,
+		                'setting' => array( 'favorite_recipes_enabled', '1' ),
+		                'deps' => array(
+		                    'jquery',
+		                ),
+		                'data' => array(
+		                    'name' => 'wpurp_favorite_recipe',
+		                    'ajaxurl' => WPUltimateRecipe::get()->helper('ajax')->url(),
+		                    'nonce' => wp_create_nonce( 'wpurp_favorite_recipe' ),
+		                )
+		            ),	  
+		            array(
+		                /*'url' => WPUltimateRecipePremium::get()->premiumUrl . '/js/add-to-shopping-list.js',*/
+		                'name' => 'custom-shopping-list',
+		                'url' => self::$_PluginUri . 'assets/js/custom_shopping_list.js',
+		                'path' => self::$_PluginPath . 'assets/js/custom_shopping_list.js',
+		                // 'premium' => true,
+		                'public' => true,
+		                'deps' => array(
+		                    'jquery',
+		                ),
+		                'data' => array(
+		                    'name' => 'wpurp_add_to_shopping_list',
+		                    'ajaxurl' => WPUltimateRecipe::get()->helper('ajax')->url(),
+		                    'nonce' => wp_create_nonce( 'wpurp_add_to_shopping_list' ),
+		                )
+		            ),
+		        );
 				// }
 			}
 			
@@ -253,7 +242,7 @@ class Custom_WPURP_Templates {
 				$this->custom_enqueued_scripts = array (
 					array(
 		                'name' => 'custom-user-submissions-list',
-		                'uri' => self::$_PluginUri . 'assets/js/',
+		                'url' => self::$_PluginUri . 'assets/js/',
 		                'path' => self::$_PluginPath . 'assets/js/',
 		                'file' => 'custom_user_submissions_list.js',
 		                'deps' => array(
@@ -273,11 +262,10 @@ class Custom_WPURP_Templates {
 			elseif ( is_page( [self::RECIPE_NEW_SLUG, self::RECIPE_EDIT_SLUG] ) ) {
 				$js_enqueue = array();
 
-		                
 				$this->custom_enqueued_scripts = array(
 					array(
 					    'name' => 'select2wpurp',
-					    'uri' => WPUltimateRecipe::get()->coreUrl . '/vendor/select2/',
+					    'url' => WPUltimateRecipe::get()->coreUrl . '/vendor/select2/',
 					    'file' => 'select2.min.js',
 					    'deps' => array(
 					        'jquery',
@@ -292,7 +280,7 @@ class Custom_WPURP_Templates {
 					),
 		            array(
 		                'name' => 'custom-user-submissions',
-		                'uri' => self::$_PluginUri . 'assets/js/',
+		                'url' => self::$_PluginUri . 'assets/js/',
 		                'path' => self::$_PluginPath . 'assets/js/',
 		                'file' => 'custom_user_submission.js',
 		                'deps' => array(
@@ -329,18 +317,33 @@ class Custom_WPURP_Templates {
 		return $array;
 	}
 
-	public function custom_wpurp_scripts_enqueue() {
+	public function custom_wpurp_scripts_styles_enqueue() {
+
+		// Enqueue Styles
+		if ( array() == $this->custom_enqueued_styles ) return;
+		foreach ( $this->custom_enqueued_styles as $key=>$style) {
+			$handler = isset($style['name'])?$style['name']:'custom_wpurp_style_' . $key;
+			// $handler = (null !== $script(['name']))?$script(['name']):'custom_wpurp_' . $key;
+			$url = isset($style['url'])?$style['url']:'';
+			$path = isset($style['path'])?$style['path']:'';
+			$file = isset($style['file'])?$style['file']:'';
+			$deps = isset($style['deps'])?$style['deps']:'';
+			$version = self::CUSTOM_WPURP_TEMPLATES_VERSION;
+			$this->custom_enqueue_style( $handler, $url, $path, $file, $deps, $version );
+  		}
+
+		// Enqueue Scripts
 		if ( array() == $this->custom_enqueued_scripts ) return;
 		foreach ( $this->custom_enqueued_scripts as $key=>$script) {
-			$handler = isset($script['name'])?$script['name']:'custom_wpurp_' . $key;
+			$handler = isset($script['name'])?$script['name']:'custom_wpurp_script_' . $key;
 			// $handler = (null !== $script(['name']))?$script(['name']):'custom_wpurp_' . $key;
-			$uri = isset($script['uri'])?$script['uri']:'';
+			$url = isset($script['url'])?$script['url']:'';
 			$path = isset($script['path'])?$script['path']:'';
 			$file = isset($script['file'])?$script['file']:'';
 			$deps = isset($script['deps'])?$script['deps']:'';
-			$version = isset($script['vers'])?$script['vers']:'';
+			$version = self::CUSTOM_WPURP_TEMPLATES_VERSION;
 			$footer = isset($script['footer'])?$script['footer']:false;
-			$this->custom_enqueue_script( $handler, $uri, $path, $file, $deps, $version, $footer );
+			$this->custom_enqueue_script( $handler, $url, $path, $file, $deps, $version, $footer );
 
 			if (isset($script['data'])) {	
 				$data_name = $script['data']['name'];
@@ -350,7 +353,22 @@ class Custom_WPURP_Templates {
   		}
 	}
 
-	public function custom_enqueue_script( $handler, $uri='', $path='', $file='', $deps='', $version='', $footer=false ) {	
+
+	public function custom_enqueue_style( $handler, $url='', $path='', $file='', $deps='', $version='' ) {	
+		if ( !strpos($file, '.min.css') ) {
+			$minfile = str_replace( '.css', '.min.css', $file );
+			if (file_exists( $path . $minfile) && WP_MINIFY ) {	
+				$file=$minfile;
+			}
+		}
+		//echo '<pre>' . "minpath = {$minpath}" . '</pre>';
+		//echo '<pre>' . "path = {$path}" . '</pre>';
+	  	//if ((url_exists($minpath)) && (WP_DEBUG==false)) {
+	    wp_enqueue_style( $handler, $url . $file, $deps, $version );
+	}
+
+
+	public function custom_enqueue_script( $handler, $url='', $path='', $file='', $deps='', $version='', $footer=false ) {	
 		if ( !strpos($file, '.min.js') ) {
 			$minfile = str_replace( '.js', '.min.js', $file );
 			if (file_exists( $path . $minfile) && WP_MINIFY ) {	
@@ -360,7 +378,7 @@ class Custom_WPURP_Templates {
 		//echo '<pre>' . "minpath = {$minpath}" . '</pre>';
 		//echo '<pre>' . "path = {$path}" . '</pre>';
 	  	//if ((url_exists($minpath)) && (WP_DEBUG==false)) {
-	    wp_enqueue_script( $handler, $uri . $file, $deps, $version, $footer );
+	    wp_enqueue_script( $handler, $url . $file, $deps, $version, $footer );
 	}
 
 	

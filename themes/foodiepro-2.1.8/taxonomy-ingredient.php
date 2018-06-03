8f3bda8770e2c01da5b8826c9615416ced6765ad
Pascal <psc.olv@gmail.com>
2018-06-03 17:22:48 +0200

    Added ingredient taxonomy page

---
+++ themes/foodiepro-2.1.8/taxonomy-ingredient.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * Genesis Framework.
+ *
+ *
+ * @package Genesis\Templates
+ * @author  StudioPress
+ * @license GPL-2.0+
+ * @link    http://my.studiopress.com/themes/genesis/
+ */
+
+add_action( 'genesis_before_content', 'genesis_do_archive_title_description' );
+//Removes Title and Description on Archive, Taxonomy, Category, Tag
+remove_action( 'genesis_before_loop', 'genesis_do_taxonomy_title_description', 15 );
+
+remove_action('genesis_loop','genesis_do_loop');
+add_action('genesis_loop','custom_loop_with_child_terms');
+
+
+/* Add child terms in Ingredients taxonomy archive
+---------------------------------------------------------------------------*/
+
+function custom_loop_with_child_terms() {
+	$obj=get_queried_object();
+	$query_args = array(
+		'post_type' => 'recipe',
+		'tax_query' => array(
+			array(
+				'taxonomy' => 'ingredient',
+				'field'    => 'slug',
+				'terms'    =>  $obj->slug,
+				'has_children'    =>  true,
+			),
+		),
+	);
+	genesis_custom_loop( $query_args );
+}
+
+
+/**
+ * Echo the title with the archive title and description
+ *
+ * @since 1.9.0
+ */
+function genesis_do_archive_title_description() {
+	echo '<div class="archive-description taxonomy-archive-description taxonomy-description">';
+	  	echo '<h1 class="archive-title">';
+			$title = '';
+			echo apply_filters( 'genesis_archive_title_text', $title ) . "\n";
+		echo '</h1>';
+		echo '<p>';
+			$description = '';
+			echo apply_filters( 'genesis_archive_description_text', $description ) . "\n";
+		echo '</p>';
+	echo '</div>';
+}
+
+
+genesis();

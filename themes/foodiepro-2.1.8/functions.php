6f7be8a91bcf9ea5b9b809f37b114d46c65421ad
Pascal <psc.olv@gmail.com>
2019-09-24 09:12:12 +0200

    Changed style to autumn

--- themes/foodiepro-2.1.8/functions.php
+++ themes/foodiepro-2.1.8/functions.php
@@ -21,7 +21,7 @@ define( 'CHILD_THEME_DEVELOPER', 'Shay Bocks' );
 define( 'CHILD_THEME_URL', get_stylesheet_directory_uri() );
 define( 'CHILD_THEME_PATH', get_stylesheet_directory() );
 
-define( 'CHILD_COLOR_THEME', 'spring' ); // christmas, autumn, winter, summer
+define( 'CHILD_COLOR_THEME', 'autumn' ); // christmas, autumn, winter, summer
 
 define( 'PLUGINS_URL', plugins_url() );
 

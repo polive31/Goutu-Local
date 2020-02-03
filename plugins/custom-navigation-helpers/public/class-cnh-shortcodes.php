d4a6711f4b51c11eef03afe040b6d13f70ce6cf4
Pascal <psc.olv@gmail.com>
2020-02-03 23:55:06 +0100

    Updated permalink shortcode according to author_base change

--- plugins/custom-navigation-helpers/public/class-cnh-shortcodes.php
+++ plugins/custom-navigation-helpers/public/class-cnh-shortcodes.php
@@ -482,7 +482,7 @@ class CNH_Shortcodes {
 				$user = get_user_by('id', $user_id);
 				if (!$user) return;
 				$token = $user->data->user_nicename;
-				$url = add_query_arg( 'author_name', $token , $url);
+				$url = add_query_arg( foodiepro_get_author_base(), $token , $url);
 				if ( !empty($type) )
 					$url = add_query_arg( 'post_type', $type, $url);
 			}

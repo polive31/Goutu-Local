5ee815a1bb1861dba945b0227c1033594aabc793
Pascal <psc.olv@gmail.com>
2020-05-12 00:45:07 +0200

    Fixed admin comments issue

--- plugins/custom-admin-helpers/admin/class-cah-post-metabox.php
+++ plugins/custom-admin-helpers/admin/class-cah-post-metabox.php
@@ -1,25 +1,23 @@
+<?php
 
+// Exit if accessed directly.
+if (!defined('ABSPATH')) {
+    exit;
+}
 
-	<?php
-
-    // Exit if accessed directly.
-    if (!defined('ABSPATH')) {
-        exit;
-    }
-
-    class CAH_Post_Metabox
+class CAH_Post_Metabox
+{
+    public function post_excerpt_meta_box()
     {
-        public function post_excerpt_meta_box()
-        {
-            add_meta_box(
-                'post_excerpt',
-                __('Post Excerpt', 'foodiepro'),
-                array($this, 'post_excerpt_meta_box_callback')
-            );
-        }
-        public function post_excerpt_meta_box_callback()
-        {
-            the_excerpt();
-        }
-
+        add_meta_box(
+            'post_excerpt',
+            __('Post Excerpt', 'foodiepro'),
+            array($this, 'post_excerpt_meta_box_callback')
+        );
     }
+    public function post_excerpt_meta_box_callback()
+    {
+        the_excerpt();
+    }
+
+}
--- plugins/custom-admin-helpers/includes/class-custom-admin-helpers.php
+++ plugins/custom-admin-helpers/includes/class-custom-admin-helpers.php
@@ -34,24 +34,24 @@ class Custom_Admin_Helpers {
         // add_action( 'admin_init',                               array($Helpers, 'prevent_plugin_update_conflicts'));
 
         $Filter = new CAH_Post_Filter();
-        add_action( 'restrict_manage_posts',    array($Filter, 'restrict_manage_posts')   );
-        add_filter( 'parse_query',              array($Filter, 'add_posts_filter' )       );
+        add_action( 'restrict_manage_posts',                    array($Filter, 'restrict_manage_posts')   );
+        add_filter( 'parse_query',                              array($Filter, 'add_posts_filter' )       );
 
         $Options = new CAH_Options();
-        add_action('admin_menu',            array($Options, 'add_foodiepro_options_page'));
-        add_action('admin_init',            array($Options, 'hydrate'), 0);
-        add_action('admin_init',            array($Options, 'register_color_theme_settings'));
-        add_action('admin_init',            array($Options, 'register_adminbar_settings'));
+        add_action('admin_menu',                                array($Options, 'add_foodiepro_options_page'));
+        add_action('admin_init',                                array($Options, 'hydrate'), 0);
+        add_action('admin_init',                                array($Options, 'register_color_theme_settings'));
+        add_action('admin_init',                                array($Options, 'register_adminbar_settings'));
 
-        add_action('admin_init',            array($Options, 'maybe_display_admin_notice'));
+        add_action('admin_init',                                array($Options, 'maybe_display_admin_notice'));
 
         $Adminbar = new CAH_Adminbar();
         // add_action( 'after_setup_theme',    array($Adminbar, 'admin_bar_visibility'));
-        add_action( 'show_admin_bar',       array($Adminbar, 'filter_admin_bar_visibility'));
-        add_filter('casm_styles_blacklist', array($Adminbar, 'remove_dashicons'));
+        add_action( 'show_admin_bar',                           array($Adminbar, 'filter_admin_bar_visibility'));
+        add_filter( 'casm_styles_blacklist',                    array($Adminbar, 'remove_dashicons'));
 
-        add_action( 'init',                 array($Adminbar, 'blockusers_init' ));
-        add_action( 'admin_bar_menu',       array($Adminbar, 'add_toolbar_items'), 999  );
+        add_action( 'init',                                     array($Adminbar, 'blockusers_init' ));
+        add_action( 'admin_bar_menu',                           array($Adminbar, 'add_toolbar_items'), 999  );
 
     }
 
